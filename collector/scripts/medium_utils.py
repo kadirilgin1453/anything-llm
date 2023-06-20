0226a9ec9f51a441efd7a6fb5149ce77d12a6f0b
Sourcery AI <>
2023-06-20 08:06:34 +0000

    'Refactored by Sourcery'

--- collector/scripts/medium_utils.py
+++ collector/scripts/medium_utils.py
@@ -4,26 +4,25 @@ from bs4 import BeautifulSoup
 def get_username(author_url):
   if '@' in author_url:
     pattern = r"medium\.com/@([\w-]+)"
-    match = re.search(pattern, author_url)
-    return match.group(1) if match else None 
   else:
     # Given subdomain
-    pattern = r"([\w-]+).medium\.com"
-    match = re.search(pattern, author_url)
-    return match.group(1) if match else None 
+    pattern = r"([\w-]+).medium\.com" 
+
+  match = re.search(pattern, author_url)
+  return match[1] if match else None 
 
 def get_docid(medium_docpath):
   pattern = r"medium\.com/p/([\w-]+)"
   match = re.search(pattern, medium_docpath)
-  return match.group(1) if match else None 
+  return match[1] if match else None 
 
 def fetch_recent_publications(handle):
   rss_link = f"https://medium.com/feed/@{handle}"
   response = requests.get(rss_link)
-  if(response.ok == False):
-    print(f"Could not fetch RSS results for author.")
+  if not response.ok:
+    print("Could not fetch RSS results for author.")
     return []
-  
+
   xml = response.content
   soup = BeautifulSoup(xml, 'xml')
   items = soup.find_all('item')
@@ -40,8 +39,7 @@ def fetch_recent_publications(handle):
       return json.load(file)
 
   for item in items:
-    tags = []
-    for tag in item.find_all('category'): tags.append(tag.text)
+    tags = [tag.text for tag in item.find_all('category')]
     content = BeautifulSoup(item.find('content:encoded').text, 'html.parser')
     data = {
       'id': get_docid(item.find('guid').text),
@@ -52,7 +50,7 @@ def fetch_recent_publications(handle):
       'pageContent': content.get_text()
     }
     publications.append(data)
-  
+
   with open(file_path, 'w+', encoding='utf-8') as json_file:
     json.dump(publications, json_file, ensure_ascii=True, indent=2)
     print(f"{len(publications)} articles found for author medium.com/@{handle}. Saved to medium-logs/medium-{handle}.json")
--- collector/scripts/youtube.py
+++ collector/scripts/youtube.py
@@ -12,7 +12,7 @@ def youtube():
     channel_link = input("Paste in the URL of a YouTube channel: ")
     channel_id = get_channel_id(channel_link)
 
-    if channel_id == None or len(channel_id) == 0:
+    if channel_id is None or len(channel_id) == 0:
         print("Invalid input - must be full YouTube channel URL")
         exit(1)
 
@@ -25,29 +25,29 @@ def youtube():
     print(f"\nFetching transcripts for {len(channel_data.get('items'))} videos - please wait.\nStopping and restarting will not refetch known transcripts in case there is an error.\nSaving results to: {transaction_output_dir}.")
     totalTokenCount = 0
     for video in alive_it(channel_data.get('items')):
-      video_file_path = transaction_output_dir + f"/video-{video.get('id')}.json"
-      if os.path.exists(video_file_path) == True:
-        continue
-       
-      formatter = TextFormatter()
-      json_formatter = JSONFormatter()
-      try:
-        transcript = YouTubeTranscriptApi.get_transcript(video.get('id'))
-        raw_text = clean_text(formatter.format_transcript(transcript))
-        duration = get_duration(json_formatter.format_transcript(transcript))
+        video_file_path = f"{transaction_output_dir}/video-{video.get('id')}.json"
+        if os.path.exists(video_file_path) == True:
+          continue
 
-        if(len(raw_text) > 0):
-          fullText = append_meta(video, duration, raw_text)
-          tokenCount = len(tokenize(fullText))
-          video['pageContent'] = fullText
-          video['token_count_estimate'] = tokenCount
-          totalTokenCount += tokenCount
-          with open(video_file_path, 'w', encoding='utf-8') as file:
-            json.dump(video, file, ensure_ascii=True, indent=4)
-      except:
-        print("There was an issue getting the transcription of a video in the list - likely because captions are disabled. Skipping")
-        continue
-    
+        formatter = TextFormatter()
+        json_formatter = JSONFormatter()
+        try:
+          transcript = YouTubeTranscriptApi.get_transcript(video.get('id'))
+          raw_text = clean_text(formatter.format_transcript(transcript))
+          duration = get_duration(json_formatter.format_transcript(transcript))
+
+          if(len(raw_text) > 0):
+            fullText = append_meta(video, duration, raw_text)
+            tokenCount = len(tokenize(fullText))
+            video['pageContent'] = fullText
+            video['token_count_estimate'] = tokenCount
+            totalTokenCount += tokenCount
+            with open(video_file_path, 'w', encoding='utf-8') as file:
+              json.dump(video, file, ensure_ascii=True, indent=4)
+        except:
+          print("There was an issue getting the transcription of a video in the list - likely because captions are disabled. Skipping")
+          continue
+
     print(f"[Success]: {len(channel_data.get('items'))} video transcripts fetched!")
     print(f"\n\n////////////////////////////")
     print(f"Your estimated cost to embed all of this data using OpenAI's text-embedding-ada-002 model at $0.0004 / 1K tokens will cost {ada_v2_cost(totalTokenCount)} using {totalTokenCount} tokens.")
